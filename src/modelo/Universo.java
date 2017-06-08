ad45c85c2d4f0a245b00ad94732e58ce59536e5d
EzeCuervo <ezecuervo.cod@gmail.com>
2017-06-08 15:22:15 -0300

    Se modifica Universo, agregando la función inicializarUniverso donde crea 5 planetas.

--- src/modelo/Universo.java
+++ src/modelo/Universo.java
@@ -3,14 +3,13 @@ package modelo;
 import java.util.*;
 
 public class Universo extends ElementoDelJuego {
-	private List <Planeta> lplanetas = new ArrayList<Planeta>();
-	private List <ElementoDelJuego> elementos = new ArrayList<ElementoDelJuego>();
-	private List <Jugador> jugadores = new ArrayList<Jugador>();
+	private List <Planeta> lplanetas;
+	private List <ElementoDelJuego> elementos;
+	private List <Jugador> jugadores;
 	public List<ElementoDelJuego> getElementos(){
 		return this.elementos;
 	}
-	
-	
+
 	public void avanzarTurno(){
 		super.avanzarTurno();
 		for (ElementoDelJuego e : this.elementos){
@@ -20,12 +19,9 @@ public class Universo extends ElementoDelJuego {
 	}
 	
 	public Universo(){
-		Planeta tierra = new Terrestre("Tierra");
-		Planeta marte = new  Tundra("Marte");
-		Planeta venus =  new Tundra ("Venus");
-		lplanetas.add(marte);
-		lplanetas.add(tierra);
-		lplanetas.add(venus);
+		jugadores = new ArrayList<Jugador>();
+		lplanetas = new ArrayList<Planeta>();
+		elementos = new ArrayList<ElementoDelJuego>();
 
 	}
 	
@@ -52,6 +48,20 @@ public class Universo extends ElementoDelJuego {
 
 	public void setJugadores(List<Jugador> jugadores) {
 		this.jugadores = jugadores;
+	}
+	
+	public void incializarUniverso(){
+		// Se crean 5 planetas
+		Planeta tierra = new Terrestre("Tierra");
+		Planeta marte = new  Terrestre("Marte");
+		Planeta venus =  new Tundra ("Venus");
+		Planeta junip = new Lava("Junip");
+		Planeta melmack = new Tundra("Melmack");
+		this.lplanetas.add(tierra);	
+		this.lplanetas.add(marte);
+		this.lplanetas.add(venus);
+		this.lplanetas.add(junip);
+		this.lplanetas.add(melmack);
 	}
 	
 }
